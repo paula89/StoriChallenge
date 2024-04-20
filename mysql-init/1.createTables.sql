d6cc6d5a97058da86a84869fb49fa04fe214831d
priverosPaxos <priveros@itbit.com>
2024-04-20 20:16:53 -0300

    update and fix.sql

--- mysql-init/1.createTables.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE IF NOT EXISTS
-        Transactions (
-              Id            varchar(50) NOT NULL
-              UserId        varchar(50) NOT NULL
-              CreationDate  date        NOT NULL
-              Debit         double      NOT NULL
-              Credit        double      NOT NULL
-       );
-
